d651b9e7d024c293e1021ef105236f66a570693c
hongdroid <hongdroid94@gmail.com>
2019-04-11 00:04:59 +0900

    AndroidManifest.xml - 시작 액티비티 Splash Activity로 변경 SplashActivity.java - 3초뒤 메인액티비티로 이동하게 핸들러구현 activity_splash.xml - 스플래시 임시 레이아웃 제작 penguin.png - 펭귄 이미지 샘플추가(by. iconfinder)

--- app/src/main/java/com/teamsquare/everyoneslinux/SplashActivity.java
+++ app/src/main/java/com/teamsquare/everyoneslinux/SplashActivity.java
@@ -1,13 +1,30 @@
 package com.teamsquare.everyoneslinux;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+
+        // 3초 딜레이를 주고 메인액티비티로 넘어가게 임시 구현
+        Handler handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        };
+        handler.sendEmptyMessageDelayed(0, 3000);
     }
 }
