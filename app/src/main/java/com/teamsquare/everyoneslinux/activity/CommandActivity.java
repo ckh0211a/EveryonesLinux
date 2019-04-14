ea632caf6c395067eabe54f7baabb28a50729851
ckh0211a <40537147+ckh0211a@users.noreply.github.com>
2019-04-14 09:14:56 +0900

    Commnad 쪽만 검색적용, 확정되면 나머지도 반영

--- app/src/main/java/com/teamsquare/everyoneslinux/activity/CommandActivity.java
+++ app/src/main/java/com/teamsquare/everyoneslinux/activity/CommandActivity.java
@@ -1,10 +1,13 @@
 package com.teamsquare.everyoneslinux.activity;
 
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -13,11 +16,15 @@ import com.teamsquare.everyoneslinux.R;
 import com.teamsquare.everyoneslinux.adapter.CommandAdapter;
 import com.teamsquare.everyoneslinux.item.CommandData;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class CommandActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private LinearLayoutManager linearLayoutManager;
     private CommandAdapter commandAdapter;
+    private ArrayList<CommandData> mArrayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +47,8 @@ public class CommandActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recycler_command);
         linearLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(linearLayoutManager);
-        commandAdapter = new CommandAdapter();
-        recyclerView.setAdapter(commandAdapter);
+//      commandAdapter = new CommandAdapter();
+//      recyclerView.setAdapter(commandAdapter);
 
     }
 
@@ -51,17 +58,21 @@ public class CommandActivity extends AppCompatActivity {
         data.setTitle("cd"); // 제목 추가
         data.setContent("디렉토리를 이동하는 코드 입니다."); // 내용란 추가
         data.setResId(R.drawable.android_sample); // 이미지 추가
-        commandAdapter.addItem(data); // 리스트 아이템 추가
+//      commandAdapter.addItem(data); // 리스트 아이템 추가
+        mArrayList.add(data);
 
 
         CommandData data2 = new CommandData();
         data2.setTitle("ls"); // 제목 추가
         data2.setContent("현재경로의 폴더 또는 파일 목록을 보여줍니다."); // 내용란 추가
         data2.setResId(R.drawable.android_sample); // 이미지 추가
-        commandAdapter.addItem(data2); // 리스트 아이템 추가
 
+//      commandAdapter.addItem(data2); // 리스트 아이템 추가
+//      commandAdapter.notifyDataSetChanged(); // 리스트 저장
 
-        commandAdapter.notifyDataSetChanged(); // 리스트 저장
+        mArrayList.add(data2);
+        commandAdapter = new CommandAdapter(mArrayList);
+        recyclerView.setAdapter(commandAdapter);
 
     }
 
@@ -69,18 +80,26 @@ public class CommandActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
 
         getMenuInflater().inflate(R.menu.search_menu, menu);
-        return true;
+
+        MenuItem menuItem = menu.findItem(R.id.search);
+        final SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                commandAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
-        if(item.getItemId() == R.id.action_search) {
-
-            Toast.makeText(this, "액션을 넣어주세요.", Toast.LENGTH_SHORT).show();
-
-            return true;
-        }
         return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/teamsquare/everyoneslinux/adapter/CommandAdapter.java
+++ app/src/main/java/com/teamsquare/everyoneslinux/adapter/CommandAdapter.java
@@ -5,6 +5,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,10 +15,18 @@ import com.teamsquare.everyoneslinux.item.CommandData;
 
 import java.util.ArrayList;
 
-public class CommandAdapter extends RecyclerView.Adapter<CommandAdapter.ItemViewHolder> {
+public class CommandAdapter extends RecyclerView.Adapter<CommandAdapter.ItemViewHolder> implements Filterable {
 
-    // adapter에 들어갈 list 입니다.
-    private ArrayList<CommandData> listData = new ArrayList<>();
+// adapter에 들어갈 list 입니다.
+//  private ArrayList<CommandData> listData = new ArrayList<>();
+
+    private ArrayList<CommandData> mArrayList;
+    private ArrayList<CommandData> mFilteredList;
+
+    public CommandAdapter(ArrayList<CommandData> arrayList) {
+        mArrayList = arrayList;
+        mFilteredList = arrayList;
+    }
 
     @NonNull
     @Override
@@ -30,18 +40,55 @@ public class CommandAdapter extends RecyclerView.Adapter<CommandAdapter.ItemView
     @Override
     public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
         // Item을 하나, 하나 보여주는(bind 되는) 함수입니다.
-        holder.onBind(listData.get(position));
+        holder.onBind(mFilteredList.get(position));
+
     }
 
     @Override
     public int getItemCount() {
         // RecyclerView의 총 개수 입니다.
-        return listData.size();
+        return mFilteredList.size();
     }
 
     public void addItem(CommandData data) {
         // 외부에서 item을 추가시킬 함수입니다.
-        listData.add(data);
+        mArrayList.add(data);
+    }
+
+    @Override
+    public Filter getFilter() {
+
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String charString = charSequence.toString();
+
+                if (charString.isEmpty()) {
+                    mFilteredList = mArrayList;
+                } else {
+
+                    ArrayList<CommandData> filteredList = new ArrayList<>();
+                    for (CommandData commandData : mArrayList) {
+                        if (commandData.getTitle().toLowerCase().contains(charString) ||
+                                commandData.getContent().toLowerCase().contains(charString)) {
+
+                            filteredList.add(commandData);
+                        }
+                    }
+                    mFilteredList = filteredList;
+                }
+
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = mFilteredList;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                mFilteredList = (ArrayList<CommandData>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
     }
 
     // RecyclerView의 핵심인 ViewHolder 입니다.
